9891c4ae44379d735a33e945b92296b2ee8f6f82
Charlie Yu <hshunyu274@gmail.com>
2024-09-22 16:55:26 +0100

    new email

--- app.py
+++ app.py
@@ -302,3 +302,7 @@ def submit_results_email():
         print(e)
         traceback.print_exc()
         return jsonify({'error': str(e)}), 500
+    
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
